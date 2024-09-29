7b38989e40cebb503076c96eae02e01a5b4d96d1
Hamid <hamidjaved615@gmail.com>
2024-09-29 12:26:34 +0500

    Setup project

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,27 @@
 import React from "react"
+import {
+  Route,
+  RouterProvider,
+  createBrowserRouter,
+  createRoutesFromElements,
+} from "react-router-dom";
+import mainPage from "./pages/mainPage";
 
 function App() {
 
+  const router = createBrowserRouter(
+    createRoutesFromElements(
+      <>
+        <Route path="/" element={<MainLayout />}>
+          <Route index element={<mainPage />} />
+        </Route>
+      </>
+    )
+  );
+
   return (
     <>
-    <div className="text-cyan-200">Hello</div>
-    <div className=""></div>
+      <RouterProvider router={router} />;
     </>
   )
 }
---
+++ src/data/webHandler.js
@@ -0,0 +1,16 @@
+export const WebHandler = async (url, verb, params) => {
+    try {
+        let options = {
+            method: verb,
+            headers: { 'Content-Type': "application/json" },
+            body: params,
+            credentials: 'include',
+        }
+        const data = await fetch(url, options)
+        const status = data.status
+        const response = await data.json()
+        return { response, status }
+    } catch (error) {
+        return error.message
+    }
+}
